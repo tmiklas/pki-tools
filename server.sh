6d9c16ffec791e657d311c7165a23d65fe78b1cd
Tomasz Miklas <tm@Tomaszs-MacBook-Pro.local>
2018-11-14 21:11:49 +0000

    Added server.sh - client (non-CA) certs use both CN and SAN fields, client.sh only CN

---
+++ server.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# (c) @tomaszmiklas 2017
+# because "PKI is hard"
+# so let's work smarter, not harder
+#
+
+# create initial serial if missing
+if [ ! -s serial ]; then
+  echo 01 > serial
+fi
+
+if [ -z $2 ]; then
+  echo "Usage: $0 <client CN> <client SAN>"
+  exit 1
+fi
+
+# autoincrement serial
+SERIAL=$((`cat serial` + 1))
+#HEXSERIAL=`printf "%02X\n" ${SERIAL}`
+
+# generate key
+openssl genrsa -des3 -out ${1}.key 4096
+
+# remove password from the key, otherwise services (web server, etc) will ask for it at boot, or fail to boot
+openssl rsa -in ${1}.key -out ${1}-NOPASS.key
+
+# create client CSR using SAN
+openssl req -new -sha256 -key ${1}-NOPASS.key -reqexts SAN -config <(cat /etc/ssl/openssl.cnf <(printf "[SAN]\nsubjectAltName=${2}")) -out ${1}.csr
+
+# self-sign
+openssl x509 -req -days 365 -extfile <(printf "subjectAltName=${2}") -in ${1}.csr -CA ca.crt -CAkey ca.key -set_serial ${SERIAL} -out ${1}.crt && echo ${SERIAL} > serial
+
+# convert to PKCS12
+# only no-password version, it's still protected with export password
+#openssl pkcs12 -export -clcerts -in ${1}.crt -inkey ${1}.key -out ${1}.p12
+openssl pkcs12 -export -clcerts -in ${1}.crt -inkey ${1}-NOPASS.key -out ${1}-NOPASS.p12
+
+# merge into single PEM - use if needed
+#echo openssl pkcs12 -in ${1}-NOPASS.p12 -out ${1}-NOPASS.pem -clcerts
